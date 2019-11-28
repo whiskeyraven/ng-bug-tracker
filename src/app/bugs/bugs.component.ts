6c9b73d3db591e7b5d049152d7703cca7899239b
tdempsey2 <tedempsey@earthlink.net>
2019-11-27 23:41:44 -0800

    Fixing sorting bug that was not a bug

--- src/app/bugs/bugs.component.ts
+++ src/app/bugs/bugs.component.ts
@@ -8,50 +8,65 @@ import { element } from 'protractor';
 export interface PeriodicElement {
   id: number;
   title: string;
-  severity: object;
-  priority: object;
-  status: object;
-  type: object;
+  severity: string;
+  priority: string;
+  status: string;
+  type: string;
   shortDesc: string;
-  owner: object;
-  fixer: object;
-  date: object;
+  owner: string;
+  fixer: string;
+  date: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {
     id: 1,
     title: 'Hydrogen',
-    severity: { severity: 'critical' },
-    priority: { priority: '1st' },
-    status: { status: 'defined' },
-    type: { type: 'cosmetic' },
+    severity: 'critical',
+    priority: '1st',
+    status: 'defined',
+    type: 'cosmetic',
     shortDesc:  'Page does not align correctly',
-    owner: { owner: 'Bob Thompson' },
-    fixer: { first: 'Tina Johnson', second: 'Eric Swanson' },
-    date: { date: '06/06/2020' }
+    owner: 'Bob Thompson',
+    fixer: 'Eric Swanson',
+    date: '06/06/2020'
   },
   {
     id: 2,
-    title: 'Test',
-    severity: { severity: 'critical' },
-    priority: { priority: '1st' },
-    status: { status: 'defined' },
-    type: { type: 'cosmetic' },
-    shortDesc:  'Test',
-    owner: { owner: 'Bob Thompson' },
-    fixer: { second: 'Tina Johnson', first: 'Eric Swanson' },
-    date: { date: '06/06/2020' }
+    title: 'test',
+    severity: 'major',
+    priority: '2nd',
+    status: 'fixing',
+    type: 'functional',
+    shortDesc:  'Page does not align',
+    owner: 'Bob Thomason',
+    fixer: 'Derick Swanson',
+    date: '06/07/2020'
+  },
+  {
+    id: 3,
+    title: 'testickles',
+    severity: 'minor',
+    priority: '3rd',
+    status: 'texting',
+    type: 'functional',
+    shortDesc:  'Page does align',
+    owner: 'Bob Thomasons',
+    fixer: 'Derick Swansons',
+    date: '06/05/2020'
+  },
+  {
+    id: 15,
+    title: 'Brian',
+    severity: '11',
+    priority: '1st',
+    status: 're-testing',
+    type: 'a11y',
+    shortDesc:  'Page does ',
+    owner: 'Henry Jacobson',
+    fixer: 'Bigus Dickus',
+    date: '06/06/2020'
   }
-  // {id: 2, title: 'Helium', severity: 'major', priority: '2nd'},
-  // {id: 3, title: 'Lithium', severity: 'moderate', priority: '3rd'},
-  // {id: 4, title: 'Beryllium', severity: 'minor', priority: '4th'},
-  // {id: 5, title: 'Boron', severity: 'WTF', priority: '11'},
-  // {id: 6, title: 'Carbon', severity: 'moderate', priority: '3rd'},
-  // {id: 7, title: 'Nitrogen', severity: 'moderate', priority: '3rd'},
-  // {id: 8, title: 'Oxygen', severity: 'moderate', priority: '3rd'},
-  // {id: 9, title: 'Fluorine', severity: 'moderate', priority: '3rd'},
-  // {id: 10, title: 'Neon', severity: 'moderate', priority: '3rd'},
 ];
 
 @Component({
@@ -61,15 +76,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class BugsComponent implements OnInit {
   displayedColumns: string[] = [ 'id', 'title', 'severity', 'priority', 'status', 'type', 'shortDesc', 'owner', 'fixer', 'date'];
-  dataSource: MatTableDataSource<PeriodicElement>;
-
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor() {
-  }
+   }
 
   ngOnInit() {
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.dataSource.sort = this.sort;
   }
 
