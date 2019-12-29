810a0c5ff18f817cde018d15b6a2b6e59a73d9f9
tdempsey2 <tedempsey@earthlink.net>
2019-12-28 17:31:42 -0800

    Fixing branch

---
+++ src/app/bug-form/bug-form.model.ts
@@ -0,0 +1,37 @@
+export interface Severity {
+  value: string;
+  name: string;
+  id: string; }
+export interface Priority {
+  value: string;
+  name: string;
+  id: string;
+ }
+export interface Status {
+  value: string;
+  name: string;
+  id: string;
+ }
+export interface BugType {
+  name: string;
+  id: string;
+  value: string;
+  // name: string[];
+  // id: string[];
+  // value: string[];
+ }
+export interface Owner {
+  value: string;
+  name: string;
+  id: string;
+ }
+export interface Fixer {
+  value: string;
+  name: string;
+  id: string;
+ }
+export interface Date {
+  value: string;
+  name: string;
+  id: string;
+ }
